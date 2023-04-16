161079e4324e94dfa00d006695f7f7647a8565a0
matt <mathewandnana@gmail.com>
2023-04-15 22:54:04 -0500

    adding read.me defining sql in connect.js file uploading video link in read.me as well

--- src/connect.js
+++ src/connect.js
@@ -1,9 +1,10 @@
+const mysql = require('mysql2');
+
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Mathew@97',
     database: 'employee_tracker_db'
 });
-
 
 module.exports = db;
